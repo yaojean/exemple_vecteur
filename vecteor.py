1b42fc23bada740bd5fa4d67fadaf25b4fa91d9a
Romeo tohoun <romeotohoun@gmail.com>
2021-12-09 19:58:35 +0000

    ajout de la methode qui fait la somme

--- vecteor.py
+++ vecteor.py
@@ -7,3 +7,12 @@ class Vecteur:
         n= math.sqrt(self.x**2+self.y**2)
     
         return n
+    def somme(self,t):
+        self.x+=t.x
+        self.y+=t.y
+
+v1 = Vecteur(4,3)
+v2 = Vecteur(3,4)
+v1.somme(v2)
+print(v1.x)
+print(v1.y)
